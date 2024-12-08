9e23b2c78be1fc7b13f0d75f88cd387579351bd0
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2024-12-09 01:56:49 +0530

    practiced

---
+++ DAY 05/Quiz/main.py
@@ -0,0 +1,25 @@
+
+import math
+
+## 1
+
+# def calculate_sphere_volume(radius):
+#     return (4)*(math.pi)*(radius**2)
+
+# print(calculate_sphere_volume(5))
+
+## 2
+
+# def sum_of_square(num): 
+#     num = range(1, num + 1)  # Generate a range of numbers from 1 to num
+#     sum = 0
+#     for i in num:
+#         sum += i**2  # Square each number and add it to sum
+#     return sum
+
+# # Example Usage
+# result = sum_of_square(5)  # Pass an integer to compute sum of squares from 1 to 5
+# print(result)  # Output: 55
+
+## 3
+
--- Practice/main.py
+++ Practice/main.py
@@ -39,3 +39,102 @@
 # animalLis1 = [["cat", "dog"], ["elephant", "giraffe"]]
 # print(animalLis1[0][1])
 
+# numberList = [1, 2, 3, 4, 5]
+# numberList.insert(2, 6)
+
+# print(numberList)
+
+# num = ("1")
+# print(type(num), num)
+
+# num1 = (1, 2, 3, 4, 5)
+# print(type(num1), num1)
+
+# rangeList = list(range(2, 101,  2))
+# print(rangeList)
+
+# thisDict = {
+#     "brand": "Ford",
+#     "model": "Mustang",
+#     "year": 1964,
+#     "coordinates": (1, 2, 3),
+#     "horsepower and toqure": [500, 600]
+# }
+
+# brand = thisDict["brand"]
+# print(brand) 
+  
+# brand1 = thisDict.get("brand")
+# print(brand1)
+
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+
+
+# student_record = {
+#     name: {
+#         "name": name,
+#         "age": age,
+#         "average": 85
+#     },
+# }
+
+# print(student_record[name], student_record[name]["name"], student_record[name]["age"], student_record[name]["average"])
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# marks = input("Enter your marks: ")
+# marks = int(marks)
+# grades = ""
+
+# if(75 <=  marks <= 100):
+#     grades = "A"
+# elif(65 <= marks < 75): 
+#     grades = "B"
+# elif(55 <= marks < 65): 
+#     grades = "C"
+# elif(35 <= marks < 55): 
+#     grades = "D"
+# elif(0 <= marks < 34): 
+#     grades = "W"
+# else:
+#     grades = ("Invalid Marks")
+
+# match marks:
+#     case 
+
+# results = {
+#     "name": name,
+#     "age": age, 
+#     "marks": marks,
+#     "grades": grades
+# }
+
+# print("name: ",results["name"], " age: ",results["age"], " marks: ",results["marks"], " grades: ",results["grades"])
+
+# fruits = ["apple", "mango", "banana"]
+
+# for i in fruits:
+#     print(i)
+
+# for j in range(0, 5):
+#     print(j, end=" \n")
+
+# list = [1, 2, 3]
+# for i in range(len(list)):
+#     print(i)    
+
+# m = 5
+# i = 0 
+
+# while i < m: 
+#     print(i, end="" )
+#     i=i+1
+# print("End")
+
+# def greet(name, greeting="Hello"):
+#     return f"{greeting} {name}!"
+
+# print(greet("Roshen"))
+
