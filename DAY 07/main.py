772ab8de0b4d757a5158ca72309b630628a84474
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2024-12-17 08:54:56 +0530

    python libraries

---
+++ DAY 07/main.py
@@ -0,0 +1,43 @@
+## What is a Python Library? Why do we use it?
+# In Python, libraries are collections of pre-writtern code and modules that provide a wide range of functionality to simplify the development of programs. 
+# These libraries contain functions, classes, and methods that can be imported and used in your Python programs, reducing the amount of code you need to write and speeding up the development process.
+
+## Module vs Library
+
+# A module is a single file that contains Python code. 
+# It can define functions, classes, and variables, and can be imported and used in other Python programs.
+# Modules are used to organize code and make it easier to manage and reuse.
+
+# A library is a collection of modules that provide a specific set of functionality.
+# Libraries can contain multiple modules, each of which provides different features and capabilities.
+# Libraries are used to extend the functionality of Python and provide additional tools and resources for developers.
+
+## Open source libraries
+# Open source libraries are libraries that are freely available for anyone to use, modify, and distribute.
+# These libraries are typically developed by a community of developers who contribute code and collaborate on projects.
+# Open source libraries are often hosted on platforms like GitHub, where users can access the source code, report issues, and contribute to the development of the library.
+
+## Selecting relavant libraries
+# When selecting libraries for your Python projects, it's important to consider the requirements of your project and the functionality you need.
+# Some factors to consider when choosing libraries include:
+# The features and capabilities provided by the library.
+# The ease of use and documentation of the library.
+# The performance and efficiency of the library.
+# The community and support around the library.
+# The licensing and compatibility of the library with your project.
+
+## Popular Python Libraries
+# There are thousands of Python libraries available, covering a wide range of topics and domains.
+# Some of the most popular Python libraries include:
+# NumPy: A library for numerical computing that provides support for arrays, matrices, and mathematical functions.
+# Pandas: A library for data manipulation and analysis that provides data structures like DataFrames and Series.
+# Matplotlib: A library for creating visualizations and plots, including line charts, bar charts, and scatter plots.
+# Requests: A library for making HTTP requests and working with APIs.
+# TensorFlow: A library for machine learning and deep learning that provides tools for building and training neural networks.
+# Django: A web framework for building web applications and APIs.
+# Flask: A lightweight web framework for building web applications and APIs.
+
+## NumPy Library
+# NumPy is a popular Python library for numerical computing that provides support for arrays, matrices, and mathematical functions.
+# NumPy, which stands for Numerical Python, is a fundamental open-source library in python thar provides support for large, multi-dimensional arrays and matrices, along with a collection of mathematical functions to operate on these arrays.    
+# NumPy is widely used in scientific computing, data analysis, and machine learning applications, and is an essential tool for working with numerical data in Python.
